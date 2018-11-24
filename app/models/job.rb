63f9df3cab39627189a7ea5daa675420626cb533
mirai <miraiqie@gmail.com>
2018-11-24 16:28:16 +0800

    implement wage part

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   validates :title, presence: true
-  valifates :description, presence: true
+  validates :description, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_upper_bound, presence: true
   validates :contact_email, presence: true
